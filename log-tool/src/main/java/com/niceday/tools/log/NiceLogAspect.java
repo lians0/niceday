1ef5f24995b1d6c14628372b8a9050c21543ece4
shuanglian2 <shuanglian2@iflytek.com>
2023-04-07 17:34:20 +0800

    feat：修改切面逻辑使用前后置通知代替环绕通知（未测试）

--- log-tool/src/main/java/com/niceday/tools/log/NiceLogAspect.java
+++ log-tool/src/main/java/com/niceday/tools/log/NiceLogAspect.java
@@ -1,16 +1,24 @@
 package com.niceday.tools.log;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+/**
+ * @author shuanglian2
+ */
 @Aspect
 @Component
 public class NiceLogAspect {
+
+    private long startTime;
+    private long endTime;
 
     /**
      * 带有返回值的日志
@@ -21,7 +29,7 @@ public class NiceLogAspect {
      * @param endTime
      * @param annotation
      */
-    private static void haveResultLog(ProceedingJoinPoint joinPoint, long startTime, Object result, long endTime, NiceLog annotation) {
+    private static void haveResultLog(JoinPoint joinPoint, long startTime, Object result, long endTime, NiceLog annotation) {
         normalLog(joinPoint, annotation);
         String className = joinPoint.getTarget().getClass().getName();
 
@@ -49,7 +57,7 @@ public class NiceLogAspect {
      * @param joinPoint
      * @param annotation
      */
-    private static void normalLog(ProceedingJoinPoint joinPoint, NiceLog annotation) {
+    private static void normalLog(JoinPoint joinPoint, NiceLog annotation) {
         // 判断是否需要打印所在类名
         if (annotation.logClass()) {
             System.out.print(joinPoint.getSignature().getDeclaringTypeName() + ".");
@@ -80,19 +88,72 @@ public class NiceLogAspect {
     }
 
     // 定义切点，用于拦截被@CustomAnnotation注解标注的方法或类
-    @Around("@annotation(com.niceday.tools.log.NiceLog) || @within(com.niceday.tools.log.NiceLog)")
-    public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+//    @Around("@annotation(com.niceday.tools.log.NiceLog) || @within(com.niceday.tools.log.NiceLog)")
+//    public Object logMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+//        // 获取目标方法所在类上的@CustomAnnotation注解
+//        NiceLog annotation = getAnnotation(joinPoint);
+//        normalLog(joinPoint, annotation);
+//        System.out.println();
+//        long startTime = System.currentTimeMillis(); // 记录方法开始执行的时间
+//        Object result = joinPoint.proceed(); // 执行目标方法
+//        long endTime = System.currentTimeMillis(); // 记录方法结束执行的时间
+//        haveResultLog(joinPoint, startTime, result, endTime, annotation);
+//
+//        return result;
+//    }
+
+    // 定义切点，用于拦截被@CustomAnnotation注解标注的方法或类
+    @Before("@annotation(com.niceday.tools.log.NiceLog) || @within(com.niceday.tools.log.NiceLog)")
+    public void logMethodBefore(JoinPoint joinPoint) {
         // 获取目标方法所在类上的@CustomAnnotation注解
         NiceLog annotation = getAnnotation(joinPoint);
         normalLog(joinPoint, annotation);
         System.out.println();
-        long startTime = System.currentTimeMillis(); // 记录方法开始执行的时间
-        Object result = joinPoint.proceed(); // 执行目标方法
-        long endTime = System.currentTimeMillis(); // 记录方法结束执行的时间
-        haveResultLog(joinPoint, startTime, result, endTime, annotation);
-
-        return result;
+        // 记录方法开始执行的时间
+        startTime = System.currentTimeMillis();
     }
+
+    @AfterReturning(pointcut = "@annotation(com.niceday.tools.log.NiceLog) || @within(com.niceday.tools.log.NiceLog)", returning = "result")
+    public void logMethodAfterReturning(JoinPoint joinPoint, Object result) {
+        // 获取目标方法所在类上的@CustomAnnotation注解
+        NiceLog annotation = getAnnotation(joinPoint);
+
+        // 记录方法结束执行的时间
+        endTime = System.currentTimeMillis();
+        haveResultLog(joinPoint, startTime, result, endTime, annotation);
+    }
+
+    @AfterThrowing(pointcut = "@annotation(com.niceday.tools.log.NiceLog) || @within(com.niceday.tools.log.NiceLog)", throwing = "exception")
+    public void logMethodAfterThrowing(JoinPoint joinPoint, Throwable exception) {
+        // 获取目标方法所在类上的@CustomAnnotation注解
+        NiceLog annotation = getAnnotation(joinPoint);
+        // 记录方法结束执行的时间
+        endTime = System.currentTimeMillis();
+        exceptionLog(joinPoint, startTime, exception, endTime, annotation);
+    }
+
+    /**
+     * 异常日志
+     *
+     * @param joinPoint  切点
+     * @param startTime  开始时间
+     * @param exception  异常
+     * @param endTime    结束时间
+     * @param annotation 注解
+     */
+    private void exceptionLog(JoinPoint joinPoint, long startTime, Throwable exception, long endTime, NiceLog annotation) {
+        normalLog(joinPoint, annotation);
+        String className = joinPoint.getTarget().getClass().getName();
+
+        System.out.print(" -> ");
+        System.out.print("exception=" + exception.getMessage());
+        // 判断是否需要打印方法执行时间
+        if (annotation.logTime()) {
+            System.out.print(", time=" + (endTime - startTime) + "ms");
+        }
+        System.out.println();
+    }
+
 
     /**
      * 获取注解值
@@ -100,7 +161,7 @@ public class NiceLogAspect {
      * @param joinPoint 切点
      * @return
      */
-    private NiceLog getAnnotation(ProceedingJoinPoint joinPoint) {
+    private NiceLog getAnnotation(JoinPoint joinPoint) {
         NiceLog annotation = (NiceLog) joinPoint.getSignature().getDeclaringType().getAnnotation(NiceLog.class);
         if (annotation == null) {// 如果类上没有注解，则获取方法上的注解
             MethodSignature signature = (MethodSignature) joinPoint.getSignature();
